04ec412601e07383d9d9619040b44b1b8ef4f097
Shankhanil Kar <karshankhanil@gmail.com>
2020-07-30 14:26:55 +0530

    Add project files.

---
+++ DeskBooker.Core/DataInterface/IDeskBookingRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using DeskBooker.Core.Domain;
+
+namespace DeskBooker.Core.DataInterface
+{
+    public interface IDeskBookingRepository
+    {
+        void Save(DeskBooking deskBooking);
+        IEnumerable<DeskBooking> GetAll();
+    }
+}
---
+++ DeskBooker.Core/Domain/DeskBookingBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DeskBooker.Core.Validation;
+
+namespace DeskBooker.Core.Domain
+{
+    public class DeskBookingBase
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        [DateInFuture]
+        [DateWithoutTime]
+        public DateTime Date { get; set; }
+    }
+}
